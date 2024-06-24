209f32a83ae15759594c458110a98b250119a50f
vn33 <vishalpnaik666@gmail.com>
2024-06-24 16:14:17 +0530

    rescaling

---
+++ rescale.py
@@ -0,0 +1,37 @@
+import cv2 as cv
+
+def rescaleFrame(frame, scale=0.75):
+    # Images, Videos, Live Videos
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+def changeRes(width, height):
+    # Live Videos
+    capture.set(3, width)
+    capture.set(4, height)
+
+img = cv.imread('Photos/cat_large.jpg')
+cv.imshow('Cat', img)
+
+resized_image = rescaleFrame(img)
+cv.imshow('Resized Cat', resized_image)
+
+
+capture = cv.VideoCapture('Videos/dog.mp4') # 0 -> webcam
+
+while True:
+    isTrue, frame = capture.read()
+
+    frame_resized = rescaleFrame(frame, scale=0.2)
+
+    cv.imshow('Video', frame)
+    cv.imshow('Video Resized', frame_resized)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
